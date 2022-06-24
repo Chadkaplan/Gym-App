f71fc9a4784abf711bbc12367d4b23455331314d
Chad Kaplan <chadkaplan@knights.ucf.edu>
2022-06-24 11:07:32 -0400

    Loader bug fixed.

--- src/components/Loader.js
+++ src/components/Loader.js
@@ -1,10 +1,17 @@
-import React from 'react';
-import { Stack } from '@mui/material';
-import { InfinitySpin } from 'react-loader-spinner';
+import React from "react";
+import { Stack } from "@mui/material";
+import { SpinnerCircular } from "spinners-react";
+
+// https://www.npmjs.com/package/spinners-react
 
 const Loader = () => (
-  <Stack direction="row" justifyContent="center" alignItems="center" width="100%">
-    <InfinitySpin color="grey" />
+  <Stack
+    direction="row"
+    justifyContent="center"
+    alignItems="center"
+    width="100%"
+  >
+    <SpinnerCircular color="red"/>
   </Stack>
 );
 
