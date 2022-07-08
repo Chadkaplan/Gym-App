d1ccf6f460f7dc7259359f861464339c9de22bb5
Chad Kaplan <chadkaplan@knights.ucf.edu>
2022-07-07 23:49:16 -0400

    Creating folder structure to separate styles in colocation directories.

--- src/components/BodyPart/BodyPart.js
+++ src/components/BodyPart/BodyPart.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { Stack, Typography } from '@mui/material';
 import Icon from '../assets/icons/gym.png';
+import  "./bodyPart.css";
 
 const BodyPart = ({ item, setBodyPart, bodyPart }) => (
   <Stack
@@ -14,7 +15,7 @@ const BodyPart = ({ item, setBodyPart, bodyPart }) => (
       window.scrollTo({ top: 1800, left: 100, behavior: 'smooth' });
     }}
   >
-    <img src={Icon} alt="dumbbell" style={{ width: '40px', height: '40px' }} />
+    <img className='BodyPart__image' src={Icon} alt="dumbbell"/>
     <Typography fontSize="24px" fontWeight="bold" fontFamily="Alegreya" color="#3A1212" textTransform="capitalize"> {item}</Typography>
   </Stack>
 );
